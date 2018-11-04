42c8d13670d2dfc03d5c646b08fbdc3532c1cac3
chasm85 <mariusz.zywicki@gmail.com>
2018-11-04 12:47:58 +0100

    Funkcja realizująca problem Collatza dla dowolnej liczby całkowitej

---
+++ Nauka/collatz.py
@@ -0,0 +1,20 @@
+def collatz(number):
+    if number % 2 == 1:
+        number = 3 * number + 1
+        print(number)
+        return number
+    elif number % 2 == 0:
+        number = number // 2
+        print(number)
+        return number
+
+
+print('Podaj liczbę: ')
+try:
+    liczba = int(input())
+    while True:
+        liczba = collatz(liczba)
+        if liczba == 1:
+            break
+except ValueError:
+    print("Podaj liczbę całkowitą")
